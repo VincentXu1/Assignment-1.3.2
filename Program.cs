b9c7095aecfcad89975e2efae2f3c78c1add60d1
Vincent Xu <vincent.xu1@outlook.com>
2025-07-09 12:42:14 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Assignment_1._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates a new string array with specified size and values.
+            string[] planets = new string[8] {
+                "Mercury",
+                "Venus",
+                "Earth",
+                "Mars",
+                "Jupiter",
+                "Saturn",
+                "Neptune",
+                "Uranus"
+            };
+            // Creates a new string array with specified values.
+            string[] months = new string[] {
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+            };
+            // Creates a new string array with specified values without using the `new` keyword.
+            string[] days = {
+                "Sunday",
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday"
+            };
+            // Creates a new string array with specified size without initially specifying values.
+            string[] time = new string[2];
+            time[0] = "am";
+            time[1] = "pm";
+        }
+    }
+}
